a1d437e0bcad851942ed3c2b83ee2b0fc80b4595
Klaudyna123 <klaudynasoczewka@wp.pl>
2020-04-03 20:18:47 +0200

    rozwiązanie zadania 1

---
+++ Zadanie2.py
@@ -0,0 +1,19 @@
+import random
+import numpy
+
+def random_wektor(n):
+    wektor_losowy = []
+    for i in range (n):
+        liczba_los = random.normalvariate(4,2)
+        wektor_losowy.append(liczba_los)
+    return wektor_losowy
+
+
+wektor_100 = random_wektor(100)
+
+srednia=numpy.mean(wektor_100)
+odchyl=numpy.std(wektor_100)
+
+print(wektor_100)
+print(srednia)
+print(odchyl)
